6a816bfd67880035389f836dac9933f3d3d7335f
Danny Vo <idannyvo14@gmail.com>
2022-01-11 22:04:46 -0500

    adding information, styling

---
+++ app.js
@@ -0,0 +1,18 @@
+const linkedin = document.querySelector("#linkedin")
+const github = document.querySelector("#github")
+
+linkedin.addEventListener("mouseover", function (e) {
+    linkedin.style.color = "#0077b5"
+})
+
+linkedin.addEventListener("mouseout", function (e) {
+    linkedin.style.color = "black"
+})
+
+github.addEventListener("mouseover", function (e) {
+    github.style.color = "#6e5494"
+})
+
+github.addEventListener("mouseout", function (e) {
+    github.style.color = "black"
+})
