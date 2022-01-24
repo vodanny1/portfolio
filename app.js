017c85caf485d64b47bef71cc4a5deba7ac0baa6
Danny Vo <idannyvo14@gmail.com>
2022-01-23 21:32:17 -0500

    adding new background image

--- app.js
+++ app.js
@@ -1,69 +1,62 @@
-const linkedin = document.querySelector("#linkedin")
-const github = document.querySelector("#github")
+const linkedin = document.querySelector("#linkedin");
+const github = document.querySelector("#github");
+const btnDark = document.querySelector("#btnDark");
 
-// Create a blank div, get the height of the absolute div
-// set the new height to be the blank div so that it separates
-// let mainDivHeight = document.getElementsByClassName('section1')[0].offsetHeight
-// let blankDiv = document.getElementsByClassName('blankDiv')[0]
-// blankDiv.style.height = mainDivHeight + 5 + "px"
-
-const theme = localStorage.getItem("data-theme")
+const theme = localStorage.getItem("data-theme");
 
 if (theme === "dark") {
-    document.documentElement.setAttribute("data-theme", "dark")
-    dark = true
-    linkedin.style.color = "white"
-    github.style.color = "white"
+  document.documentElement.setAttribute("data-theme", "dark");
+  dark = true;
+  linkedin.style.color = "white";
+  github.style.color = "white";
 } else {
-    dark = false
-    document.documentElement.setAttribute("data-theme", "light")
-    linkedin.style.color = "black"
-    github.style.color = "black"
+  dark = false;
+  document.documentElement.setAttribute("data-theme", "light");
+  linkedin.style.color = "black";
+  github.style.color = "black";
 }
 
 function toggleDark() {
-    if (!dark) {
-        document.documentElement.setAttribute("data-theme", "dark")
-        localStorage.setItem("data-theme", "dark")
-        linkedin.style.color = "white"
-        github.style.color = "white"
-    } else {
-        document.documentElement.setAttribute("data-theme", "light")
-        localStorage.setItem("data-theme", "light")
-        linkedin.style.color = "black"
-        github.style.color = "black"
-    }
-    dark = !dark
+  if (!dark) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("data-theme", "dark");
+    linkedin.style.color = "white";
+    github.style.color = "white";
+    btnDark.style.color = "white";
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("data-theme", "light");
+    linkedin.style.color = "black";
+    github.style.color = "black";
+    btnDark.style.color = "black";
+  }
+  dark = !dark;
 }
 
-const btnDark = document.querySelector("#btnDark")
-
 btnDark.addEventListener("click", function (e) {
-    toggleDark()
-})
+  toggleDark();
+});
 
 linkedin.addEventListener("mouseout", function (e) {
-    if (dark) {
-        linkedin.style.color = "white"
-    } else {
-        linkedin.style.color = "black"
-    }
-})
+  if (dark) {
+    linkedin.style.color = "white";
+  } else {
+    linkedin.style.color = "black";
+  }
+});
 
 github.addEventListener("mouseout", function (e) {
-    if (dark) {
-        github.style.color = "white"
-    } else {
-        github.style.color = "black"
-    }
-})
+  if (dark) {
+    github.style.color = "white";
+  } else {
+    github.style.color = "black";
+  }
+});
 
 linkedin.addEventListener("mouseover", function (e) {
-    linkedin.style.color = "#0077b5"
-})
+  linkedin.style.color = "#0077b5";
+});
 
 github.addEventListener("mouseover", function (e) {
-    github.style.color = "#6e5494"
-})
-
-
+  github.style.color = "#6e5494";
+});
