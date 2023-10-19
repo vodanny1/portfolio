d12379b713a87aa742afacbe6c8a443ff58ca808
vodanny1 <idannyvo14@gmail.com>
2023-10-19 12:13:20 -0400

    changed script file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
-import { RouterModule, Routes } from '@angular/router';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { NgModule } from '@angular/core';
-import { HomepageComponent } from './homepage/homepage.component';
-import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes } from "@angular/router";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { NgModule } from "@angular/core";
+import { HomepageComponent } from "./homepage/homepage.component";
+import { ContactComponent } from "./contact/contact.component";
 
 const appRoutes: Routes = [
-  { path: '', component: WelcomeComponent },
-  { path: 'home', component: HomepageComponent },
-  { path: 'contact', component: ContactComponent },
+  { path: "", component: WelcomeComponent },
+  { path: "home", component: HomepageComponent },
+  { path: "contact", component: ContactComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
